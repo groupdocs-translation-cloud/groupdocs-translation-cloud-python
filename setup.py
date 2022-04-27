378809a9a1000ff965ec97ff26f22be0cc482229
Alena Panyugina <alena.panyugina@aspose.com>
2022-04-27 09:51:16 +0300

    Update to 22.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from os import path
 
 NAME = "groupdocs-translation-cloud"
-VERSION = "22.2"
+VERSION = "22.4"
 # To install the library, run the following
 #
 # python setup.py install
